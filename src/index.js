232a1f4921e6d62e55b77003d473cd1b413f478b
Benjamin Goh <masotime@gmail.com>
2020-04-05 09:29:05 -0700

    Remove module in package.json, simplify index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,30 @@
 import decorate from './decorator';
 
-const COMPONENTS = [
-	'AttributionControl',
-	'Circle',
-	'CircleMarker',
-	'ControlledLayer',
-	'DivOverlay',
-	'FeatureGroup',
-	'GeoJSON',
-	'GridLayer',
-	'ImageOverlay',
-	'LayerGroup',
-	'LayersControl',
-	'Map',
-	'MapComponent',
-	'MapControl',
-	'MapEvented',
-	'MapLayer',
-	'Marker',
-	'Pane',
-	'Path',
-	'Polygon',
-	'Polyline',
-	'Popup',
-	'Rectangle',
-	'ScaleControl',
-	'TileLayer',
-	'Tooltip',
-	'VideoOverlay',
-	'WMSTileLayer',
-	'ZoomControl'
-];
-
-const COMPONENT_MAP = COMPONENTS.reduce((map, name) => {
-	map[name] = decorate(name);
-	return map;
-}, {});
-
-module.exports = COMPONENT_MAP;
+export const AttributionControl = decorate('AttributionControl')
+export const Circle = decorate('Circle')
+export const CircleMarker = decorate('CircleMarker')
+export const ControlledLayer = decorate('ControlledLayer')
+export const DivOverlay = decorate('DivOverlay')
+export const FeatureGroup = decorate('FeatureGroup')
+export const GeoJSON = decorate('GeoJSON')
+export const GridLayer = decorate('GridLayer')
+export const ImageOverlay = decorate('ImageOverlay')
+export const LayerGroup = decorate('LayerGroup')
+export const LayersControl = decorate('LayersControl')
+export const Map = decorate('Map')
+export const MapComponent = decorate('MapComponent')
+export const MapControl = decorate('MapControl')
+export const MapEvented = decorate('MapEvented')
+export const MapLayer = decorate('MapLayer')
+export const Marker = decorate('Marker')
+export const Pane = decorate('Pane')
+export const Path = decorate('Path')
+export const Polygon = decorate('Polygon')
+export const Polyline = decorate('Polyline')
+export const Popup = decorate('Popup')
+export const Rectangle = decorate('Rectangle')
+export const ScaleControl = decorate('ScaleControl')
+export const TileLayer = decorate('TileLayer')
+export const Tooltip = decorate('Tooltip')
+export const VideoOverlay = decorate('VideoOverlay')
+export const WMSTileLayer = decorate('WMSTileLayer')
