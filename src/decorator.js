6510b5345faa6049971987745a0a0eb41541c7d5
Benjamin Goh <masotime@gmail.com>
2020-04-05 10:46:25 -0700

    Create a ref by default and attach leafletElement to the decorated component if available

--- src/decorator.js
+++ src/decorator.js
@@ -11,6 +11,8 @@ export default function decorate(componentName) {
 			super(props);
 			this.state = { loaded: false };
 			this.constructor.displayName = displayName;
+			this._leafletRef = React.createRef();
+			this._getRef = () => this.props.leafletRef || this._leafletRef;
 		}
 
 		componentDidMount() {
@@ -18,15 +20,22 @@ export default function decorate(componentName) {
 			this.ClientComponent = require('react-leaflet')[componentName];
 		}
 
+		componentDidUpdate() {
+			const ref = this._getRef();
+			if (ref && ref.current) {
+				this.leafletElement = ref.current.leafletElement;
+			}
+		}
+
 		render() {
 			if (!this.state.loaded) return null;
 
 			const { ClientComponent } = this;
-			const { children, leafletRef, ...rest } = this.props;
+			const { children, ...rest } = this.props;
 			const childComponents = typeof children === 'function' ? children() : children;
 
 			return (
-				<ClientComponent {...rest} ref={leafletRef}>
+				<ClientComponent {...rest} ref={this._getRef()}>
 					{ childComponents }
 				</ClientComponent>
 			);
